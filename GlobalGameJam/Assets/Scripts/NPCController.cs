8e44ac1b8993bc6e26d3272760599941c0634c66
Rachid Telfort <rachidt1994@gmail.com>
2019-01-26 16:25:30 -0500

    Added daytime functionality so players can get visual cues that their time is running out.

---
+++ GlobalGameJam/Assets/Scripts/NPCController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public Rigidbody NPCRigidBody;
+    float yLimit = 0;
+    bool up = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NPCRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animate();
+    }
+
+    void Animate()
+    {
+        if(up)
+        {
+            ++yLimit;
+
+            if(yLimit==1)
+            {
+                up = false;
+            }
+        }
+
+        else
+        {
+            --yLimit;
+
+            if(yLimit==0)
+            {
+                up = true;
+            }
+        }
+       
+
+        
+        //else if(yLimit > 0)
+        NPCRigidBody.AddForce(new Vector3(0, yLimit, 0));
+    }
+}
